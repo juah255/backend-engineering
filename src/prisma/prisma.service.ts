4b323d6fd906796637f191db0b6df115a4602cd4
juah255 <junayedahammed94@gmail.com>
2026-05-26 21:00:19 +0600

    integrate prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
@@ -6,6 +8,15 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(config: ConfigService) {
+    super({
+      adapter: new PrismaPg({
+        connectionString: config.getOrThrow<string>('DATABASE_URL'),
+      }),
+      log: ['warn', 'error'],
+    });
+  }
+
   async onModuleInit(): Promise<void> {
     await this.$connect();
   }
